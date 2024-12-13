52ea01948b7000ad560b06a968425d63394d024a
Luis Rosales <conejoconsal@hotmail.com>
2024-12-13 17:13:04 -0600

    rt

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   experimental: {
-     appDir: true, 
-   },
    images: {
      domains: ['picsum.photos', 'plchldr.co', "via.placeholder.com", "firebasestorage.googleapis.com", "lh3.googleusercontent.com"], // Dominios permitidos para imágenes
    },
